77b109cf05ffd2f8ef2d633403c546373f29944a
zengxingqi <me@zengxingqi.com>
2022-11-13 17:50:10 +0800

    feat: init

--- index.mjs
+++ index.mjs
@@ -43,18 +43,18 @@ function ioConnection(client) {
     // console.log("用户 %O 离开房间： %O", client.id, room);
     client.to(room).emit("leaved", room, client.id);
   }
-  async function disconnect() { // reason
+  async function disconnect(reason) {
     const room = await pubClient.get(client.id);
     // console.log("用户 %O 断开连接: %o, 房间： %O", client.id, reason, room);
     room && logout(room);
   }
   function login(room) {
-    // console.log("用户 %O 登陆房间： %O", client.id, room);
-    // 给自己发
     const myRoom = nameSpaced.adapter.rooms.get(room);
+    // console.log("用户 %O 登陆房间： %O, 房间人数：%O", client.id, room, myRoom);
     if (myRoom) {
-      client.emit("online", room, [...myRoom.keys()]);
       client.to(room).emit("logged", room, client.id);
+      // 给自己发
+      client.emit("online", room, [...myRoom.keys()]);
     }
   }
   function logout(room) {
