bc213d82e1f162f591d973f4c378434d7bb05363
zengxingqi <me@zengxingqi.com>
2022-11-13 15:59:37 +0800

    feat: init

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
       "error_file": "./logs/im.err.log",
       "out_file": "./logs/im.out.log",
       "log_date_format": "YYYY-MM-DD HH:mm Z",
-      "instances" : "max",
-      "exec_mode" : "cluster",
+      // "instances" : "max",
+      // "exec_mode" : "cluster",
       "watch": false,
       "env": {
         "NODE_ENV": "production"
